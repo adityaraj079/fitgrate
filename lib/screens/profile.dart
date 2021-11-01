bd0fe2c6740d0d6ffd877ef57924082c00833c73
himanshu06d <hd156681@gmail.com>
2021-11-01 17:51:17 +0530

    profile updated

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -4,59 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-<<<<<<< HEAD
-class Profile extends StatelessWidget{
-  
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-@override
-  Widget build(BuildContext context){
-    final user = FirebaseAuth.instance.currentUser!;
-    return Scaffold(
-      appBar: AppBar(
-       title: Text('Profile'),
-       centerTitle: true,
-       actions: [TextButton( 
-         child: Text('Logout'),
-       onPressed: (){
-         
-         
-         final provider = Provider.of<GoogleSignInProvider>(context, listen:false);
-         provider.logout();
-       },
-       )
-       ],
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          color: Colors.blueGrey.shade900,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Profile',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 32),
-              CircleAvatar(
-                radius: 40,
-                backgroundImage: NetworkImage(user.photoURL!)
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Name  '+ user.displayName!
-                ,
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              SizedBox(height: 8 ),
-              Text(
-                'email  '+ user.email!,
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-
-            ],
-          ),
-        ),
-=======
 class Profile extends StatefulWidget {
   @override
   State<Profile> createState() => _ProfileState();
@@ -128,7 +75,6 @@ class _ProfileState extends State<Profile> {
           ],
         ),
       ),
->>>>>>> c69ab7b6e560506e36ad3a55da2de362fa5f5edf
     );
   }
 }
